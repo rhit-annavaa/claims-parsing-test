73656d2ebb9b582875b7bced0755de77df701cf9
rhit-annavaa <annavaa@rose-hulman.edu>
2025-07-11 12:40:17 -0700

    Top_k sentences summary attmept
    Added a second method of selecting sentences

--- scratchnltktest.py
+++ scratchnltktest.py
@@ -48,7 +48,7 @@ def setscores(text, freq_table):
     sentence_scores = {} #create a set for the scores
 
     for sentence in text: #search the input text for each sentence
-        word_count = 0 #tracks the words that are in the frequency table
+        word_count = 0 #tracks the important words' number of occurences in the sentence
         for word in word_tokenize(sentence.lower()): #iterate over the now word tokenized input, also lowercase
             if word in freq_table: #if the word is in the table
                 sentence_scores[sentence] = sentence_scores.get(sentence, 0) + freq_table[word] #add the word's frequency including its possible additional weight
@@ -58,20 +58,52 @@ def setscores(text, freq_table):
 
     return sentence_scores #return
 
-def getsummary(sentence_scores, orig_sentence, threshold = 0.6): #our threshold is set to 60% of the maximum score achieved
-    max_score = max(sentence_scores.values()) #set the max score to the highest score in the sentence_scores list
-    threshold = max_score * threshold #multiply to determine the threshold
+#Threshold based sorting
 
-    summary_sentences = [] #these are the sentences we will actually return
-    for sentence, score in sentence_scores: #checks if the score of a given sentence is higher than the threshold
-        if score >= threshold:
-            summary_sentences.append(sentence) #if it is, then add it to the list of summaries
+# def getsummary(sentence_scores, orig_sentence, threshold = 0.6): #our threshold is set to 60% of the maximum score achieved
+#     max_score = max(sentence_scores.values()) #set the max score to the highest score in the sentence_scores list
+#     threshold = max_score * threshold #multiply to determine the threshold
+#
+#     summary_sentences = [] #these are the sentences we will actually return
+#     for sentence, score in sentence_scores.items(): #checks if the score of a given sentence is higher than the threshold
+#         if score >= threshold:
+#             summary_sentences.append(sentence) #if it is, then add it to the list of summaries
+#
+#     first_sentence = orig_sentence[0] #i I want to force the program to add the first sentence to the beginning, sometimes
+#     if first_sentence not in summary_sentences: #the program will deem the first sentence below the threshold, and we end up
+#         summary_sentences.insert(0, first_sentence) #losing that sentence which has a lot of important context in most cases
+#
+#     return ' '.join(summary_sentences) #lastly we are just joining everyting together with a ' ' inbetween each phrase
 
-    first_sentence = orig_sentence[0] #i I want to force the program to add the first sentence to the beginning, sometimes
-    if first_sentence not in summary_sentences: #the program will deem the first sentence below the threshold, and we end up
-        summary_sentences.insert(0, first_sentence) #losing that sentence which has a lot of important context in most cases
+#top-k based soting
+def getsummary(sentence_scores, orig_sentences, top_k=9):
+    top_sentences = heapq.nlargest(top_k, sentence_scores.items(), key=lambda x:x[1]) #we look for
+    #the highest top-k scoring sentences. Then we want to sort the items by the score
 
-    return ' '.join(summary_sentences) #lastly we are just joining everyting together with a ' ' inbetween each phrase
+    summary_sentences = [] #creates a list of sentence strings
+    for s in top_sentences:
+        sentence = s[0] #selects the sentence not the score
+        summary_sentences.append(sentence) #adds the sentnece to the string
+
+    sorted_summary_sentences = [] #go back thru the list
+    for s in orig_sentences:
+        if s in summary_sentences:
+            sorted_summary_sentences.append(s) #add the sentence to the list if it
+            #exists in the top sentences. we do this because then the original order is preserved instead of top-k order
+
+    first_sentence = orig_sentences[0] #i I want to force the program to add the first sentence to the beginning, sometimes
+    if first_sentence not in sorted_summary_sentences: #the program will deem the first sentence below the threshold, and we end up
+        sorted_summary_sentences.insert(0, first_sentence) #losing that sentence which has a lot of important context in most cases
+
+    final_summary = ''
+    for sentence in sorted_summary_sentences:
+        final_summary += sentence + ' '
+
+    return final_summary
+
+
+
+
 
 if __name__ == "__main__": #main method
     paragraph = input("Enter a paragraph or claim: ") #user input instead of json
@@ -85,5 +117,5 @@ if __name__ == "__main__": #main method
         print(f"{score:.2f}: {sent}")                               #for sentnceses in order so I can
                                                                     #get a better idea of whats going on
     print("\nSummary:") #print out the summary to the cmd line
-    print(summary) 
+    print(summary)
 
